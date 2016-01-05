d3b40f3304c3de1dd96383e0d74141693e054271
Zoe Jablow <zoaton@gmail.com>
2016-01-05 17:51:21 -0500

    back to 8080

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ mongoose.connect('mongodb://localhost/BigMac', function (err) {
 });
 
 //Set up the port to listen
-app.listen(80, function () {
+app.listen(8080, function () {
   console.log('App listening on port 8080...');
 });
 
